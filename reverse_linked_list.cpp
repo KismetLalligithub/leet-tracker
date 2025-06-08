e1d12daccf292fb43511a69ab7b8f29095c3b967
KismetLalligithub <lalli.kismet@gmail.com>
2025-06-07 20:59:34 -0700

    Started linked list and logged problems

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,10 @@
+ListNode* reverseList(ListNode* head) {
+	if (!head || !head->next) return head; 
+
+	ListNode* newHead = reverseList(head->next); 
+
+	head->next->next = head; 
+	head->next = nullptr; 
+
+	return newHead; 
+} 
