1e151f6820b932c01c410977af6c0b1b138d3fa2
KismetLalligithub <lalli.kismet@gmail.com>
2025-07-12 17:42:17 -0700

    Fixed num islands dfs

--- num_islands_dfs.py
+++ num_islands_dfs.py
@@ -1,14 +1,16 @@
 def numIslands_dfs(grid): 
     if not grid or not grid[0]: 
         return 0 
-
+    
+    rows, cols = len(grid), len(grid[0])
     visited = set()
     islands = 0 
 
     def dfs(r, c): 
         if (r < 0 or r >= rows or c < 0 or c >= cols or (r, c) in visited or grid[r][c] == '0'): 
             return 
-
+        
+        visited.add((r, c))
         dfs(r + 1, c)
         dfs(r - 1, c)
         dfs(r, c + 1)
