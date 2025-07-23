044ab5fd55dd9284e16ec1cba241004b20d94300
Kismet Lalli <lalli.kismet@gmail.com>
2025-07-23 16:10:25 -0700

    Finished clone graph with no reference/guidance

---
+++ test_three.py
@@ -0,0 +1,23 @@
+class Node: 
+    def __init__(val, neighbors): 
+        self.val = val
+        self.neighbors = neighbors if neighbors else []
+
+def clone_graph(node): 
+    visited = {}
+
+    def dfs(n): 
+        if not n: 
+            return None
+
+        if n in visited: 
+            return visited[n]
+
+        copy = Node(n.val)
+        visited[n] = copy
+
+        for neighbor in n.neighbors: 
+            copy.neighbors.append(dfs(neighbor))
+        return copy
+
+    return dfs(node)
